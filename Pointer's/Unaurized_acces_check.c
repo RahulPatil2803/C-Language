bcc45567da2d95055deb27937c3924bfabc775b0
RahulPatil2803 <160585889+RahulPatil2803@users.noreply.github.com>
2024-08-28 21:32:57 +0530

    Update Unaurized_acces_check.c

--- Pointer's/Unaurized_acces_check.c
+++ Pointer's/Unaurized_acces_check.c
@@ -5,8 +5,8 @@ void main(){
 
 
 int a=10;
-printf("%d\n",a);
-int *ptr=&a;
-*ptr=100;
-printf("%d",a);
+printf("%d\n",a);   //original value of 'a'
+int *ptr=&a; //Here *ptr will be locate only adress of 'a' which may be any value which are assigned by the OS 
+*ptr=100;    //Here we assign value 100 in location of *ptr which are actually variable a
+printf("%d",a); //After updating value of 'a'
 }
